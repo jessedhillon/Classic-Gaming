ac6b7f622b7e4db05b84a7b2f7678a1c3eea3f8b
Jesse Dhillon <jesse@devazero.com>
2011-03-24 01:56:18 -0700

    Action window opens with relevant ROM.

--- classic_gaming.py
+++ classic_gaming.py
@@ -1,12 +1,19 @@
 import os
+import mc
 from xml.etree import ElementTree
 
+State = {}
+Views = {}
+
 config = None
+windows = {}
 
 def initialize(path='config.xml'):
     global config
     config = parse_config(os.getcwd() + '/' + path)    
-    print "results of config> " + str(config)
+
+    windows['main'] = mc.GetWindow(14000)
+    windows['action'] = mc.GetWindow(14401)
 
 def get_system_list():
     global config
@@ -21,7 +28,7 @@ def get_rom_list():
     global config
 
     roms = []
-    for slug, system in config.items():
+    for slug, system in config.iteritems():
         if 'roms' in system:
             for rom in system['roms']:
                 roms.append({'name': rom['name'],
@@ -29,12 +36,15 @@ def get_rom_list():
                              'thumbnailPath': rom.get('thumbnailPath', ''),
                              'publisher': rom.get('publisher', ''),
                              'year': rom.get('year', ''),
+                             'description': rom.get('description', ''),
                              'system': slug})
 
     return roms
 
 def launch_rom(system, romPath):
+    print "launching rom"
     global config
+    print "got config"
     os.system("%s '%s'" % (config[system]['emulatorPath'], romPath))
 
 def parse_config(path):
@@ -66,7 +76,7 @@ def parse_config(path):
         rom['name'] = r.find('name').text
         rom['path'] = r.find('path').text
 
-        for prop in ['thumbnailPath', 'publisher', 'year']:
+        for prop in ['thumbnailPath', 'publisher', 'year', 'description']:
             el = r.find(prop)
 
             if el != None:
@@ -75,3 +85,75 @@ def parse_config(path):
         systems[system].setdefault('roms', []).append(rom)
 
     return systems
+
+def get_window(name):
+    return windows[name]
+
+def unload():
+    Views = {}
+    State = {}
+    windows = {}
+    config = None
+
+class MainView(object):
+    @staticmethod
+    def get_instance():
+        global Views
+        if 'main' not in Views:
+            Views['main'] = MainView()
+        return Views['main']
+
+    def __init__(self):
+        initialize('config.xml')
+        self.window = get_window('main')
+        self.item_list = self.window.GetList(14055)
+
+        items = mc.ListItems()
+        for rom in get_rom_list():
+          item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+          item.SetLabel(rom['name'])
+          item.SetPath(rom['path'])
+          item.SetThumbnail(rom['thumbnailPath'])
+          item.SetProperty('description', rom['description'])
+          item.SetProperty('system', rom['system'])
+          item.SetProperty('publisher', rom['publisher'])
+          item.SetProperty('year', rom['year'])
+          items.append(item)
+
+        self.item_list.SetItems(items)
+
+    def on_list_item_click(self):
+        items = self.item_list.GetItems()
+        index = self.item_list.GetFocusedItem()
+
+        item = items[index]
+        State['current_item'] = item
+
+        action = ActionView.get_instance()
+        # launch_rom(item.GetProperty('system'), item.GetPath())
+
+class ActionView(object):
+    @staticmethod
+    def get_instance():
+        global Views
+        if 'action' not in Views:
+            Views['action'] = ActionView()
+        return Views['action']
+
+    def __init__(self):
+        mc.ActivateWindow(14401)
+
+        self.item = State['current_item']
+        self.window = get_window('action')
+        self.item_list = self.window.GetList(5000)
+
+        items = mc.ListItems()
+        items.append(self.item)
+        self.item_list.SetItems(items)
+
+        system = config[self.item.GetProperty('system')]
+        self.window.GetLabel(14101).SetLabel(self.item.GetProperty('publisher'))
+        self.window.GetLabel(14102).SetLabel('[COLOR grey]' + self.item.GetProperty('year') + '[/COLOR]')
+        self.window.GetLabel(14104).SetLabel(self.item.GetProperty('description'))
+        self.window.GetLabel(6013).SetLabel(system['name'])
+        self.window.GetLabel(6742).SetLabel('[COLOR grey]Path: ' + self.item.GetPath() + '[/COLOR]')
